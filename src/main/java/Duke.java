1d026826a0d3d436be16cbfa286b6efa904f966f
benardotang <benardotang@gmail.com>
2020-08-21 18:40:08 +0800

    Complete Level 0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
+
+    private static final String MESSAGE_BOUNDARY = "\n____________________________________________________________\n";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println(MESSAGE_BOUNDARY + "Hello! I'm Duke\n" + "What can I do for you?" + MESSAGE_BOUNDARY);
+        System.out.println("Bye. Hope to hear from you soon!"+ MESSAGE_BOUNDARY);
     }
 }
